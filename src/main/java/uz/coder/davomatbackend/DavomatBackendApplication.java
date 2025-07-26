cd418e0a25d2d84ee7a09374c0fb80291799a6e7
Developer-Mustafo <mustaforahimov30@gmail.com>
2025-07-26 17:00:55 +0500

    create project

---
+++ src/main/java/uz/coder/davomatbackend/DavomatBackendApplication.java
@@ -0,0 +1,13 @@
+package uz.coder.davomatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavomatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DavomatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/coder/davomatbackend/DavomatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.coder.davomatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavomatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
