817c29fd34ed0a32fb68635af1785affe8a52767
Developer-Mustafo <mustaforahimov30@gmail.com>
2025-12-25 22:17:26 +0500

    set-up database

--- src/test/java/uz/coder/davomatbackend/DavomatBackendApplicationTests.java
+++ src/test/java/uz/coder/davomatbackend/DavomatBackendApplicationTests.java
@@ -3,11 +3,20 @@ package uz.coder.davomatbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Bu test Spring Boot ilovasining context’ini to‘g‘ri yuklayaptimi
+ * tekshiradi. Agar context yuklanmasa, test xato beradi.
+ */
 @SpringBootTest
 class DavomatBackendApplicationTests {
 
+    /**
+     * Ilova konteksini yuklash testi.
+     * Agar ilova to‘g‘ri ishga tushsa, test muvaffaqiyatli o‘tadi.
+     */
     @Test
-    void contextLoads() {
+    void testContextLoads() {
+        // Test ichida hech narsa qilinmaydi, faqat context yuklanishi tekshiriladi
     }
 
 }
